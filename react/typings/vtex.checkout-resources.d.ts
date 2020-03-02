891bb468149316e94b94cd0851aef7c1b9aea965
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-02 14:26:51 -0300

    add identification form component implementation

---
+++ react/typings/vtex.checkout-resources.d.ts
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-duplicates, import/order */
+
+declare module 'vtex.checkout-resources/QueryProfile' {
+  import { DocumentNode } from 'graphql'
+
+  const query: DocumentNode
+  export default query
+}
+
+declare module 'vtex.checkout-resources/MutationUpdateOrderFormProfile' {
+  import { DocumentNode } from 'graphql'
+
+  const query: DocumentNode
+  export default query
+}
